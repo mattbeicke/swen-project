3f5d7461aa2dbcf2a5f7d436e4bf35f2f50e09de
Anthony Ficalora <amf5916@rit.edu>
2025-09-19 17:06:35 -0400

    Add some boilerplate to CupboardController, waiting for other files to be updated before proceeding.

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/CupboardController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/CupboardController.java
@@ -1,5 +1,68 @@
 package com.ufund.api.ufundapi.controller;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ufund.api.ufundapi.model.Need;
+import com.ufund.api.ufundapi.persistence.CupboardDAO;
+
+/**
+ * Handles the REST API requests for the Cupboard resource
+ * <p>
+ * {@literal @}RestController Spring annotation identifies this class as a REST
+ * API method handler to the Spring framework
+ * 
+ * @author Anthony Ficalora
+ */
+
+@RestController
+@RequestMapping("cupboard")
 public class CupboardController {
-    
+    private static final Logger LOG = Logger.getLogger(CupboardController.class.getName());
+    private CupboardDAO cupboardDAO;
+
+    /**
+     * Creates a REST API controller to reponds to requests
+     * 
+     * @param heroDao The {@link HeroDAO Hero Data Access Object} to perform CRUD operations
+     * <br>
+     * This dependency is injected by the Spring Framework
+     */
+    public CupboardController(CupboardDAO cupboardDAO) {
+        this.cupboardDAO = cupboardDAO;
+    }
+
+    /**
+     * Responds to the GET request for a {@linkplain Need need} for the given id
+     * 
+     * @param id The id used to locate the {@link Need need}
+     * 
+     * @return ResponseEntity with {@link Need need} object and HTTP status of OK if found<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Need> getNeed(@PathVariable int id) {
+        LOG.info("GET /heroes/" + id);
+        try {
+            return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
