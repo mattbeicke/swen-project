2ef6320f264421ce1d59e54eabc94031358ab549
Matthew Beicke <mwb6617@rit.edu>
2025-09-20 12:14:31 -0400

    Added updateNeed Privated data fields Added getters for all data fields

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
@@ -1,13 +1,34 @@
 package com.ufund.api.ufundapi.model;
 
 public class Need {
-    String name;
-    int id;
-    String description;
+    private String name;
+    private int id;
+    private String description;
 
-    public Need(String name, int id, String description){
+    public Need(String name, int id, String description) {
         this.name = name;
         this.id = id;
         this.description = description;
+    }
+
+    public void updateNeed(String newName, String newDesc) {
+        if (newName != null) {          // Only update what is new
+            this.name = newName;
+        }
+        if (newDesc != null) {
+            this.description = newDesc;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId(){
+        return id;
     }
 }
