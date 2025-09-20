ae71df8ae0b598c5f7269ce8c5b84226464ea0da
Matthew Beicke <mwb6617@rit.edu>
2025-09-20 12:01:55 -0400

    Added data fields and three argument constructor

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
@@ -1,5 +1,13 @@
 package com.ufund.api.ufundapi.model;
 
 public class Need {
-    
+    String name;
+    int id;
+    String description;
+
+    public Need(String name, int id, String description){
+        this.name = name;
+        this.id = id;
+        this.description = description;
+    }
 }
