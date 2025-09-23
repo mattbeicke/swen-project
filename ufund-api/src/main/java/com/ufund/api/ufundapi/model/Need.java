c0fe51dca78c05c075aa0c9370cf251e7bd7e7db
Matthew Beicke <mwb6617@rit.edu>
2025-09-23 17:38:00 -0400

    Added JsonProperty for each of the variables for use in DAO later. Added toString

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
@@ -1,18 +1,26 @@
 package com.ufund.api.ufundapi.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Need {
+    @JsonProperty("name")
     private String name;
+    @JsonProperty("id")
     private int id;
+    @JsonProperty("description")
     private String description;
 
-    public Need(String name, int id, String description) {
+    static final String STRING_FORMAT = "Need [id=%d, name=%s, description=%s]";
+
+    public Need(@JsonProperty("name") String name, @JsonProperty("id") int id,
+            @JsonProperty("description") String description) {
         this.name = name;
         this.id = id;
         this.description = description;
     }
 
     public void updateNeed(String newName, String newDesc) {
-        if (newName != null) {          // Only update what is new
+        if (newName != null) { // Only update what is new
             this.name = newName;
         }
         if (newDesc != null) {
@@ -28,7 +36,12 @@ public class Need {
         return description;
     }
 
-    public int getId(){
+    public int getId() {
         return id;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(STRING_FORMAT, id, name, description);
     }
 }
