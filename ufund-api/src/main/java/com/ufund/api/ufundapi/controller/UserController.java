adbd65827de7197bf0380b37ed96824559c982ea
Matthew Beicke <mwb6617@rit.edu>
2025-09-24 17:58:14 -0400

    added some functions to usercontroller

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/UserController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/UserController.java
@@ -1,5 +1,132 @@
 package com.ufund.api.ufundapi.controller;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ufund.api.ufundapi.model.User;
+import com.ufund.api.ufundapi.persistence.UserDAO;
+
+/**
+ * Handles the REST API requests for the User resource
+ * <p>
+ * {@literal @}RestController Spring annotation identifies this class as a REST
+ * API method handler to the Spring framework
+ * 
+ * @author Matthew Beicke
+ */
+
+@RestController
+@RequestMapping("cupboard")
 public class UserController {
+    private static final Logger LOG = Logger.getLogger(UserController.class.getName());
+    private UserDAO userDAO;
+
+    /**
+     * Creates a REST API controller to reponds to requests
+     * 
+     * @param cupboardDAO The {@link CupboardDAO Cupboard Data Access Object} to perform CRUD operations
+     * <br>
+     * This dependency is injected by the Spring Framework
+     */
+    public UserController(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
     
+    /**
+     * Creates a {@linkplain User user} with the provided user object
+     * 
+     * @param user - The {@link User user} to create
+     * 
+     * @return ResponseEntity with created {@link User user} object and HTTP status of CREATED<br>
+     * ResponseEntity with HTTP status of CONFLICT if {@link User user} object already exists<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @PostMapping("")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        LOG.info("POST /user " + user);
+        
+        try {
+            return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+
+            User newuser = userDAO.createUser(user);
+                if(newuser == null) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+            }
+            return new ResponseEntity<User>(user, HttpStatus.CREATED);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Updates the {@linkplain User user} with the provided {@linkplain User user} object, if it exists
+     * 
+     * @param user The {@link User user} to update
+     * 
+     * @return ResponseEntity with updated {@link User user} object and HTTP status of OK if updated<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @PutMapping("")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        LOG.info("PUT /user " + user);
+
+        try {
+            return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+
+            User update = userDAO.updateUser(user);
+            if(update == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(update, HttpStatus.OK);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Deletes a {@linkplain User user} with the given id
+     * 
+     * @param id The id of the {@link User user} to deleted
+     * 
+     * @return ResponseEntity HTTP status of OK if deleted<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUsers(@PathVariable int id) {
+        LOG.info("DELETE /user/" + id);
+
+        try {
+            return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+
+            boolean deleted = userDAO.deleteUser(id);
+            if(!deleted) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
