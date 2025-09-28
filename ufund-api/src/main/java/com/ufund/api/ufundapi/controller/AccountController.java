34f73bebcad68a5803b9b7db4d1bb1227f2c065b
Anthony Ficalora <amf5916@rit.edu>
2025-09-28 18:07:56 -0400

    Add vertification functionality. Currently only implemented in accounts/test. See live_demo.ipynb for details

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/AccountController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/AccountController.java
@@ -1,29 +1,20 @@
 package com.ufund.api.ufundapi.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Map;
-import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.swing.text.html.HTML;
-
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ufund.api.ufundapi.model.Need;
-import com.ufund.api.ufundapi.model.User;
-import com.ufund.api.ufundapi.persistence.CupboardDAO;
 import com.ufund.api.ufundapi.persistence.UserDAO;
 
 @RestController
@@ -36,25 +27,47 @@ public class AccountController {
         this.userDAO = userDAO;
     }
 
-    private String generateKey() {
-        Random rand = new Random();
-        int myRandomNumber = rand.nextInt(0x69999999) + 0x10000000;
-        return Integer.toHexString(myRandomNumber);
-    }
-
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody Map<String,String> input) {
+    public ResponseEntity<String> login(@RequestBody Map<String, String> input) {
         try {
             String username = input.get("username");
             String password = input.get("password");
-            boolean validLogin = userDAO.verifyLogin(username, password);
-            if(!validLogin) return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 
-            String new_key = generateKey();
-            return new ResponseEntity<>(new_key, HttpStatus.OK);
+            if (!userDAO.verifyLogin(username, password))
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 
-        } catch(IOException e) {
-            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            String key = userDAO.attemptLogin(username, password);
+            return new ResponseEntity<>(key, HttpStatus.OK);
+
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@RequestBody String username) {
+        try {
+            userDAO.attemptLogout(username);
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(@RequestBody String username, @RequestHeader Map<String, String> headers) {
+        try {
+            String key = headers.get("key");
+            if (!userDAO.verifyKey(username, key)) {
+                return new ResponseEntity<>("Invalid API key.", HttpStatus.UNAUTHORIZED);
+            }
+            return new ResponseEntity<>("API key is valid.", HttpStatus.OK);
+
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
--- ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/UserFileDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/UserFileDAO.java
@@ -3,7 +3,9 @@ package com.ufund.api.ufundapi.persistence;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -17,6 +19,8 @@ import com.ufund.api.ufundapi.model.User;
 public class UserFileDAO implements UserDAO {
 
     private static final Logger LOG = Logger.getLogger(UserFileDAO.class.getName());
+    private static final int KEY_CHARACTERS = 32;
+
     Map<Integer, User> users; // Provides a local cache of the need objects
     // so that we don't need to read from the file
     // each time
@@ -25,10 +29,12 @@ public class UserFileDAO implements UserDAO {
     // to the file
     private static int nextId;
     private String filename; // Filename to read from and write to
+    private Map<Integer, String> activeLogins;
 
     public UserFileDAO(@Value("${users.file}") String filename, ObjectMapper objectMapper) throws IOException {
         this.filename = filename;
         this.objectMapper = objectMapper;
+        this.activeLogins = new HashMap<>();
         load();
     }
 
@@ -101,7 +107,8 @@ public class UserFileDAO implements UserDAO {
 
     @Override
     public User createUser(User user) throws IOException {
-        if(getUserByUsername(user.getUsername()) != null) return null;
+        if (getUserByUsername(user.getUsername()) != null)
+            return null;
         User newUser = new User(user.getId(), user.getUsername(), user.getPassword());
         users.put(newUser.getId(), newUser);
         return newUser;
@@ -110,8 +117,10 @@ public class UserFileDAO implements UserDAO {
     @Override
     public User updateUser(User user) throws IOException {
         User existingUser = getUserByUsername(user.getUsername());
-        if(existingUser != null && existingUser != user) return null; 
-        // only continues if the username is open (owned by the same user or not taken at all)
+        if (existingUser != null && existingUser != user)
+            return null;
+        // only continues if the username is open (owned by the same user or not taken
+        // at all)
         int id = user.getId();
         User oldUser = users.get(id);
         oldUser.updateUser(user.getUsername(), user.getPassword());
@@ -126,7 +135,42 @@ public class UserFileDAO implements UserDAO {
 
     @Override
     public boolean verifyLogin(String username, String password) throws IOException {
-        if (getUserByUsername(username) == null) return false;
+        if (getUserByUsername(username) == null)
+            return false;
         return (getUserByUsername(username).getPassword().equals(password));
+    }
+
+    private String createLoginKey() {
+        Random rand = new Random();
+        String key = "";
+        for (int i = 0; i < KEY_CHARACTERS; i++) {
+            key = key + Integer.toHexString(rand.nextInt(16));
+        }
+        return key;
+    }
+
+    @Override
+    public String attemptLogin(String username, String password) throws IOException {
+        User user = getUserByUsername(username);
+        if(user == null) return null;
+        if(!verifyLogin(username, password)) return null;
+        String new_key = createLoginKey();
+        activeLogins.put(user.getId(), new_key);
+        return new_key;
+    }
+
+    @Override
+    public boolean verifyKey(String username, String key) throws IOException {
+        User user = getUserByUsername(username);
+        if(user == null) return false;
+        if(!activeLogins.containsKey(user.getId())) return false;
+        return activeLogins.get(user.getId()).equals(key);
+    }
+
+    @Override
+    public void attemptLogout(String username) throws IOException {
+        User user = getUserByUsername(username);
+        if(user == null) return;
+        activeLogins.remove(user.getId());
     }
 }
