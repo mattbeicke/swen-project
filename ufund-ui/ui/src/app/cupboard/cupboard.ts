1f68cb58737373f2c4863836c08df75402f3fae6
Matthew Beicke <mwb6617@rit.edu>
2025-10-07 13:17:10 -0400

    Updated how Needs are displayed, uses a table now. Button in table now reacts to what the user is (manager or helper), changing its text accordingly

--- ufund-ui/ui/src/app/cupboard/cupboard.ts
+++ ufund-ui/ui/src/app/cupboard/cupboard.ts
@@ -7,7 +7,7 @@ import {
   NgFor,
   UpperCasePipe,
 } from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cupboard',
@@ -17,18 +17,29 @@ import {FormsModule} from '@angular/forms';
 })
 export class Cupboard {
   selectedNeed?: Need;
+  bText = "";
 
   onSelect(need: Need): void {
     this.selectedNeed = need;
+    if (localStorage.getItem("role") == "manager") {
+      //TODO: open edit/delete modal
+    } else {
+      //TODO: add to basket
+    }
   }
-  
+
   constructor(private needService: NeedService) { }
   needs: Need[] = [];
   getNeeds(): void {
     this.needService.getNeeds()
       .subscribe(needs => this.needs = needs);
   }
-  ngOnInit(): void { 
+  ngOnInit(): void {
     this.getNeeds();
+    if (localStorage.getItem("role") == "manager") {
+      this.bText = "Edit/Delete";
+    } else {
+      this.bText = "Add to Basket";
+    }
   }
 }
