8bdc0d3628d0c7a457c5e61790866e4bf8350a00
Anthony Ficalora <amf5916@rit.edu>
2025-10-08 16:10:51 -0400

    Add error messages for login page

--- ufund-ui/ui/src/app/login/login.ts
+++ ufund-ui/ui/src/app/login/login.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { AccountsService } from '../accountservice'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,22 +13,43 @@ export class Login {
   @Input() username?: string;
   @Input() password?: string;
   key?: string;
-  
-  constructor(private accountsService: AccountsService) {}
+  message?: string;
+
+  constructor(private accountsService: AccountsService, private router: Router) { }
 
   login(): void {
-    if(this.username == null || this.password == null) {
-      return; // todo: more constructive erroring
+    if (!this.username) { // missing or removed username
+      this.message = "Missing username"
+      return;
+    }
+    if (!this.password) { // missing or removed password
+      this.message = "Missing password"
+      return;
     }
     const saved_username = this.username; // so you log in as the user you logged into, not what happened to be entered when it got processed
     this.accountsService.login(this.username, this.password)
-      .subscribe(data => (this.finalizeLogin(data, saved_username)));
+      .subscribe({
+        next: data => (this.finalizeLogin(data, saved_username)),
+        error: error => {switch(error.status){
+          case 401:
+            this.message = "Invalid username or password";
+            break;
+          case 500:
+            this.message = "Internal server error";
+            break;
+          default:
+            this.message = "Unknown error, is server online?";
+        }}
+      });
   }
 
   finalizeLogin(data: string, username: string): void {
-    localStorage.setItem('key', data);
+    localStorage.setItem('key', "");
     localStorage.setItem('username', username);
     localStorage.setItem('role', username == 'admin' ? 'manager' : 'helper');
-    // todo: switch page to cupboard, maybe?
+
+    this.router.navigate(['/cupboard']);
   }
+
+  
 }
