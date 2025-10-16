875085b04f37165d38357079200bf1379e79d9ef
Matthew Beicke <mwb6617@rit.edu>
2025-10-16 15:01:55 -0400

    Creating an account on the login page now works

--- ufund-ui/ui/src/app/accountservice.ts
+++ ufund-ui/ui/src/app/accountservice.ts
@@ -2,32 +2,39 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { rstr_md5 } from './md5';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountsService {
-  constructor(
-    private http: HttpClient) { }
-  private needsURL = 'http://localhost:8080/accounts';
+  constructor(private http: HttpClient) { }
+
+  private accountURL = 'http://localhost:8080/accounts';
+  private userURL = 'http://localhost:8080/user';
+
   login(username: string, password: string): Observable<string> {
-    return this.http.post(this.needsURL+'/login', {username: username, password: password}, {responseType: 'text'});
+    return this.http.post(this.accountURL + '/login', { username: username, password: password }, { responseType: 'text' });
     // todo: replace password with encrypt(password): this may have side effects, must discuss
   }
 
   logout(username: string, key: string): void {
-    this.http.post(this.needsURL+'/logout', username, {responseType: 'text', 'headers': {'key': key}});
+    this.http.post(this.accountURL + '/logout', username, { responseType: 'text', 'headers': { 'key': key } });
   }
 
   test(username: string, key: string): Observable<string> {
-    return this.http.get(this.needsURL+'/test/'+username, {responseType: 'text', 'headers': {'key': key}});
+    return this.http.get(this.accountURL + '/test/' + username, { responseType: 'text', 'headers': { 'key': key } });
   }
 
   getInfo(username: string, key: string): Observable<string> {
-    return this.http.get(this.needsURL+'/info/'+username, {responseType: 'text', 'headers': {'key': key}});
+    return this.http.get(this.accountURL + '/info/' + username, { responseType: 'text', 'headers': { 'key': key } });
   }
 
   encrypt(password: string): string {
     return rstr_md5(password);
+  }
+
+  createAccount(user: User) {
+    return this.http.post(this.userURL, user);
   }
 }
