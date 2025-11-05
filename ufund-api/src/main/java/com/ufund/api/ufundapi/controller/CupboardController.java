19b2ef6589a13ebdd175dfccb2803a05a596508a
Matthew Beicke <mwb6617@rit.edu>
2025-11-05 16:04:38 -0500

    Removed add/update/delete needs from cupboard controller as they are now secured behind being a manager

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/CupboardController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/CupboardController.java
@@ -6,12 +6,8 @@ import java.util.logging.Logger;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -117,87 +113,6 @@ public class CupboardController {
         LOG.info("GET /cupboard/?name=" + name);
         try {
             return new ResponseEntity<Need[]>(cupboardDAO.searchNeeds(name), HttpStatus.OK);
-        } catch (IOException e) {
-            LOG.log(Level.SEVERE, e.getLocalizedMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    /**
-     * Creates a {@link Need need} with the provided {@link Need need} object
-     * 
-     * @param need The {@link Need need} to create
-     * 
-     * @return ResponseEntity with created {@link Need need} object and HTTP status
-     *         of CREATED. ResponseEntity with HTTP status of CONFLICT if
-     *         {@link Need need} object already exists. ResponseEntity with HTTP
-     *         status of INTERNAL_SERVER_ERROR otherwise.
-     *         Only returns HTTP status of FORBIDDEN when not in development mode.
-     */
-    @PostMapping("")
-    public ResponseEntity<Need> createNeed(@RequestBody Need need) {
-        LOG.info("POST /cupboard " + need);
-
-        try {
-            Need new_need = cupboardDAO.createNeed(need);
-            if (new_need == null) {
-                return new ResponseEntity<>(HttpStatus.CONFLICT);
-            }
-            return new ResponseEntity<Need>(need, HttpStatus.CREATED);
-        } catch (IOException e) {
-            LOG.log(Level.SEVERE, e.getLocalizedMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    /**
-     * Updates the {@link Need need} with the provided {@link Need need} object, if
-     * it exists
-     * 
-     * @param need The {@link Need need} to update
-     * 
-     * @return ResponseEntity with updated {@link Need need} object and HTTP status
-     *         of OK if updated. ResponseEntity with HTTP status of NOT_FOUND if not
-     *         found. ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR
-     *         otherwise.
-     *         Only returns HTTP status of FORBIDDEN when not in development mode.
-     */
-    @PutMapping("")
-    public ResponseEntity<Need> updateNeed(@RequestBody Need need) {
-        LOG.info("PUT /cupboard " + need);
-
-        try {
-            Need update = cupboardDAO.updateNeed(need);
-            if (update == null) {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-            return new ResponseEntity<>(update, HttpStatus.OK);
-        } catch (IOException e) {
-            LOG.log(Level.SEVERE, e.getLocalizedMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    /**
-     * Deletes a {@link Need need} with the given id
-     *
-     * @param id The id of the {@link Need need} to deleted
-     *
-     * @return ResponseEntity HTTP status of OK if deleted. ResponseEntity with HTTP
-     *         status of NOT_FOUND if not found. ResponseEntity with HTTP status of
-     *         INTERNAL_SERVER_ERROR otherwise.
-     *         Only returns HTTP status of FORBIDDEN when not in development mode.
-     */
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Need> deleteNeeds(@PathVariable int id) {
-        LOG.info("DELETE /cupboard/" + id);
-
-        try {
-            boolean deleted = cupboardDAO.deleteNeed(id);
-            if (!deleted) {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-            return new ResponseEntity<>(HttpStatus.OK);
         } catch (IOException e) {
             LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/UserController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/UserController.java
@@ -8,7 +8,6 @@ import java.util.logging.Logger;
 
 import com.ufund.api.ufundapi.persistence.CompletedNeedDAO;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
